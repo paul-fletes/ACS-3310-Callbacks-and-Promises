8ad5c1334dd1ae99253682fbd72d8bd6f9b7a0ed
Paul Fletes <paul.fletes@gmail.com>
2023-09-13 12:19:14 -0500

    complete promise 4

--- Promise-4/index.js
+++ Promise-4/index.js
@@ -1,14 +1,14 @@
 const food = new Promise((resolve, reject) => {
-  
+  resolve('Enchiladas Verdes');
 })
 
 
-// resolve food here: 
+// resolve food here:
+food.then(favoriteFood => console.log(`I love ${favoriteFood}!`))
 
 
+// **Challenge 1** Resolve your promise with the name of your favorite food.
+// Do this by calling the `resolve` with an argument.
 
-// **Challenge 1** Resolve your promise with the name of your favorite food. 
-// Do this by calling the `resolve` with an argument. 
-
-// **Challenge 2** Resolve the promise `food` with the `.then()` syntax 
+// **Challenge 2** Resolve the promise `food` with the `.then()` syntax
 // and print your favorite food to the console. 
